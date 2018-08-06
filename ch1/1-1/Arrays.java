a1daba135ddb208193681d11506a1d4becbcd0b6
timtingwei <timtingwei@hotmail.com>
2018-08-06 15:57:40 +0800

    java数组

---
+++ ch1/1-1/Arrays.java
@@ -0,0 +1,15 @@
+
+public class Arrays {
+  public static void main(String[] args) {
+    int N = 5;
+    double[] a;
+    a = new double[N];
+    for (int i = 0; i < N; i++) {
+      // a[i] = 0.0;
+      StdOut.println(a[i]);
+    }
+    int[] b = {1, 1, 2, 3, 5, 8};
+  }
+}
+
+
---
+++ ch1/1-1/TestStdOut.java
@@ -0,0 +1,10 @@
+public class TestStdOut {
+  public static void main(String[] args) {
+    int a = 17;
+    int b = 23;
+    int sum = a + b;
+    StdOut.println("Hello, World");
+    StdOut.printf("%d + %d = %d\n", a, b, sum);
+  }
+}
+  
