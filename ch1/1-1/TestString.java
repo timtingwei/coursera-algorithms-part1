f3459b8b16956396ef50b575f9a3a62b943c6a4b
timtingwei <timtingwei@hotmail.com>
2018-08-06 17:40:54 +0800

    字符串转数字

---
+++ ch1/1-1/TestString.java
@@ -0,0 +1,9 @@
+
+public class TestString {
+  public static void main(String[] args) {
+    System.in.readInts()
+    String s1 = "123"; int a = 7;
+    System.out.printf("parseInt(s1) = %d\n", Integer.parseInt(s1));
+    System.out.printf("toString(a) = %s\n", Integer.toString(a));
+  }
+}
