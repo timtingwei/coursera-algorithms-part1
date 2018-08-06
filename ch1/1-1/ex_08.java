8782a9353f23a14d1cca2e12019360b0a4efda48
timtingwei <timtingwei@hotmail.com>
2018-08-06 22:53:57 +0800

    字符与字符相加, 字符与数字相加

---
+++ ch1/1-1/ex_08.java
@@ -0,0 +1,17 @@
+// Copyright [2018] <mituh>
+
+public class ex_08 {
+  public static void main(String args[]) {
+    System.out.println('b');
+    System.out.println('b' + 'c');
+    System.out.println((char) ('a' + 4));
+  }
+  
+}
+
+/*
+$ java ex_08
+b
+197
+e
+*/
