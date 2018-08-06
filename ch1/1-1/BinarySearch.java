2140b8961d6f954a265eac5c7cf81625e8135884
timtingwei <timtingwei@hotmail.com>
2018-08-06 21:15:09 +0800

    二分查找测试

---
+++ ch1/1-1/BinarySearch.java
@@ -0,0 +1,26 @@
+// Copyright [2018] <mituh>
+import java.util.Arrays;
+public class BinarySearch {
+  public static int rank(int[] a, int key) {
+    int lo = 0;
+    int hi = a.length - 1;
+    while (lo <= hi) {
+      int mid = lo + (hi - lo) / 2;
+      if      (key > a[mid]) lo = mid + 1;
+      else if (key < a[mid]) hi = mid - 1;
+      else                    return mid;
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    int[] whitelist = In.readInts(args[0]);
+    Arrays.sort(whitelist);
+    while (!StdIn.isEmpty()) {
+      int key = StdIn.readInt();
+      if (rank(whitelist, key) < 0)
+        System.out.println(key);
+    }
+  }
+}
+
