544c00fd267370ac6488d3aea4db32ede5e86d19
timtingwei <timtingwei@hotmail.com>
2018-08-13 23:16:07 +0800

    插入排序去掉show()调用

--- ch2/2-1/Insertion.java
+++ ch2/2-1/Insertion.java
@@ -28,7 +28,6 @@ public class Insertion {
     for (int i = 1; i < a.length; i++) {
       for (int j = i; j > 0 && less(a[j], a[j-1]); j--) {
         exch(a, j, j-1);
-        show(a);
       }
     }
   }
